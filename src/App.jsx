5e65dd659732fe0886e7a98686d34b26ef206087
prabanjanjeyasankar <prabanjan@gmail.com>
2024-04-14 01:17:35 +0530

    ADD. Created the Navigation Bar Component.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import NavBarComponent from './assets/Components/NavBarComponent/NavBarComponent'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <NavBarComponent/>
+      </div>
+    </>
+  )
+}
+
+export default App
