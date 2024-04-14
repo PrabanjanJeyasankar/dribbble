0ee10782490da82b482e5f09d6e80cb0156ea9f3
prabanjanjeyasankar <prabanjan@gmail.com>
2024-04-15 00:23:08 +0530

    ADD. Created the Landing page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './App.css'
 import NavBarComponent from './assets/Components/NavBarComponent/NavBarComponent'
+import HeroComponent from './assets/Components/HeroComponent/HeroComponent'
 
 function App() {
-
   return (
     <>
       <div>
-      <NavBarComponent/>
+        <NavBarComponent />
+        <HeroComponent />
       </div>
     </>
   )
